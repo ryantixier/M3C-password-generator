b4330325b02cea9ae9718d1efaf68ac526c069ed
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-05-19 21:44:20 -0600

    (JS) refactored code so boolean values could be accessed properly, deleted old code, got it working

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,11 @@
-// KEY(S) ✅
-// KEY(S) ✅
-const letters = "abcdefghijklmnopqrstuvwxyz";
-const numberRange = "0123456789";
-const specialCharacters = "~!%^&*-=+";
+// VARIABLES/INDECES
 
-var lowercase = Array.from(letters);
-var uppercase = Array.from(letters.toUpperCase());
-var number = Array.from(numberRange);
-var character = Array.from(specialCharacters);
-var randomChar = lowercase.concat(uppercase, number, character);
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var numberRange = "0123456789";
+var specialCharacters = "~!%^&*-=+";
 
-// INTRODUCTION ✅
-// INTRODUCTION ✅
+// INTRODUCTION
+// INTRODUCTION
 alert(
   "¡Bienvenidos! I'm here to generate a password for you; you'll be asked a series of questions, and then my robo-brain will spin some wheels to get you a solid password. Let's get started!"
 );
@@ -19,10 +13,10 @@ alert(
 //
 //
 
-// PASSWORD LENGTH ✅
-// PASSWORD LENGTH ✅
+// PASSWORD LENGTH
+// PASSWORD LENGTH
 
-// PROMPT ✅
+// PROMPT
 var lengthPrompt = prompt(
   "How many characters would you like in your password? (Choose a number between 8-128; whole numbers only - no decimals)."
 );
@@ -37,7 +31,7 @@ while (lengthPrompt > 128 || lengthPrompt < 8) {
     "How many characters would you like in your password? (Choose a number between 8-128; whole numbers only - no decimals."
   );
 }
-// CONFIRMATION ✅
+// CONFIRMATION
 alert(
   "You have chosen " + lengthPrompt + " characters for your password length."
 );
@@ -56,10 +50,10 @@ var lowLetterPrompt = confirm(
 // CONFIRMATION
 if (lowLetterPrompt === true) {
   alert("You have chosen to inlcude lowercase letters.");
-  lowercase;
+  var lowercase = Array.from(letters);
 } else {
   alert("You have chosen to exclude lowercase letters.");
-  lowercase = "";
+  var lowercase = "";
 }
 
 //
@@ -76,10 +70,10 @@ var upLetterPrompt = confirm(
 // CONFIRMATION
 if (upLetterPrompt === true) {
   alert("You have chosen to inlcude uppercase letters.");
-  uppercase;
+  var uppercase = Array.from(letters.toUpperCase());
 } else {
   alert("You have chosen to exclude uppercase letters.");
-  uppercase = "";
+  var uppercase = "";
 }
 
 //
@@ -89,8 +83,18 @@ if (upLetterPrompt === true) {
 // INCLUDE NUMBERS?
 
 // PROMPT
+var numberPrompt = confirm(
+  "Would you like your password to include numbers? Hit 'okay' to include; hit 'cancel' to exclude."
+);
 
 // CONFIRMATION
+if (numberPrompt === true) {
+  alert("You have chosen to inlcude numbers.");
+  var number = Array.from(numberRange);
+} else {
+  alert("You have chosen to exclude numbers.");
+  var number = "";
+}
 
 //
 //
@@ -99,14 +103,18 @@ if (upLetterPrompt === true) {
 // INLCUDE SPECIAL CHARACTERS?
 
 // PROMPT
+var specCharPrompt = confirm(
+  "Would you like your password to include special characters? Hit 'okay' to include; hit 'cancel' to exclude."
+);
 
 // CONFIRMATION
-
-//
-//
-
-// VALIDATE SELECTIONS
-// VALIDATE SELECTIONS
+if (specCharPrompt === true) {
+  alert("You have chosen to inlcude special characters.");
+  var character = Array.from(specialCharacters);
+} else {
+  alert("You have chosen to exclude special characters.");
+  var character = "";
+}
 
 //
 //
@@ -118,11 +126,12 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var password = "";
+  var randomChar = lowercase.concat(uppercase, number, character);
   for (i = 0; i < parseInt(lengthPrompt); i++) {
     var passwordRandom = Math.floor(
       Math.random() * parseInt(randomChar.length)
     );
-    password += randomChar[passwordRandom]; // ???
+    password += randomChar[passwordRandom];
   }
   return password;
 }
@@ -134,88 +143,3 @@ function writePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
-
-//
-//
-//
-//
-
-// var upLetterPrompt = confirm(
-//   "Would you like your password to include uppercase letters? Hit 'okay' to include; hit 'cancel' to exclude."
-// );
-// // upLetterPrompt: stores value for user's uppercase preferences
-// // boolean
-
-// if (upLetterPrompt === true) {
-//   alert("You have chosen to inlcude uppercase letters.");
-// } else {
-//   alert("You have chosen to exclude uppercase letters.");
-// }
-// validates according to user input's condition
-
-var numberPrompt = confirm(
-  "Would you like your password to include numbers? Hit 'okay' to include; hit 'cancel' to exclude."
-);
-// numberPrompt: stores value for user's number preferences
-// boolean
-
-if (numberPrompt === true) {
-  alert("You have chosen to inlcude numbers.");
-} else {
-  alert("You have chosen to exclude numbers.");
-}
-// validates according to user input's condition
-
-var specCharPrompt = confirm(
-  "Would you like your password to include special characters? Hit 'okay' to include; hit 'cancel' to exclude."
-);
-// specCharPrompt: stores value for user's special character preferences
-// boolean
-
-if (specCharPrompt === true) {
-  alert("You have chosen to inlcude special characters.");
-} else {
-  alert("You have chosen to exclude special characters.");
-}
-
-var generateBtn = document.querySelector("#generate");
-// creates refernce (variable) linked to <button> element id 'generate' in document object
-
-// WORK IN PROGRESS //
-function generatePassword() {
-  // DECLARES FUNCTION generatePassword
-  var finalPassword = "";
-  // DECLARES VARIABLE finalPassword AS AN EMPTY STRING
-  for (i = 0; i < parseInt(lengthPrompt); i++) {
-    // <for> ([index starting point]; [range]; [increment])
-    // cycles through lengthPrompt string index until # of iterations = lengthPrompt value
-    // parseInt: recalls string value from lengthPrompt and converts it to an integer
-    var passwordRandom = Math.floor(
-      Math.random() * parseInt(randomChar.length)
-    );
-    // creates variable to represent randomized
-    // :: Math.random() :: method (?) to randomly select numbers w/in 'Math' object
-    // :: parseInt(randomChar.length) :: converts randChar from string to numerical values;
-    //                                :: string items become referenceable by numerical place in array
-    // :: Math.floor(...*...) :: method (?) to ....{{{idk}}}.... the equation
-    // LOOK INTO THIS MORE - ASK BEN OR JUSTIN
-    finalPassword += randomChar[passwordRandom];
-    // LOOK INTO THIS MORE TOO... PLS - ASK BEN OR JUSTIN
-  }
-  return finalPassword;
-  // OUTPUT VALUE FOR finalPassword
-}
-// console.log(finalPassword); currently (2/13/23, 8:52PM) says finalPassword value is underfined... T.T
-
-function writePassword() {
-  // DECLARES FUNCTION writePassword as a command
-  var password = generatePassword();
-  // DECLARES VARIABLE password; when called, will run generatePassword function above
-  var passwordText = document.querySelector("#password");
-  // creates refernce (variable) linked to <textarea> element id 'password' in document object
-  passwordText.value = password;
-  // provides placeholder text ("password"), disappears when generateBtn is selected
-}
-
-generateBtn.addEventListener("click", writePassword);
-// creates event listener 'click'; runs writePassword function once "generate" button is clicked
